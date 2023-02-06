62a9694645a7f25ff5b044405830aab75950da34
Jason Farrell <imjason@gmail.com>
2023-02-06 13:38:22 -0500

    moved to using log analytics module

--- main.tf
+++ main.tf
@@ -1,12 +1,20 @@
 
-resource azurerm_log_analytics_workspace this {
+module "loganalytics" {
+  source  = "app.terraform.io/Kyndryl-CIO/loganalytics/azurerm"
+  version = "0.0.3"
+
   for_each                  = local.log_analytics_workspaces
 
-  name                = "log-${each.value["application"]}-${each.value["purpose"]}${each.value["environment"]}${module.regioncodes.region_codes[lower(replace(each.value["location"], " ", ""))]}${each.value.instance == "" ? "" : "${each.value.instance}"}"
-  location            = each.value.location
-  resource_group_name = each.value.resource_group_name
-  sku                 = each.value.sku
-  retention_in_days   =  each.value.retention_days
+  location              = each.value.location
+  resource_group_name   = each.value.resource_group_name
+  sku                   = each.value.sku
+  retention_in_days     = each.value.retention_days
+  required_tags         = {}
+  additional_tags       = {}
+  application           = each.value.application
+  purpose               = each.value.purpose
+  environment           = each.value.environment
+  instance              = each.value.instance
 }
 
 resource azurerm_application_insights this {
@@ -15,6 +23,6 @@ resource azurerm_application_insights this {
   name                = "appi-${each.value["application"]}-${each.value["purpose"]}${each.value["environment"]}${module.regioncodes.region_codes[lower(replace(each.value["location"], " ", ""))]}${each.value.instance == "" ? "" : "${each.value.instance}"}"
   location            = each.value.location
   resource_group_name = each.value.resource_group_name
-  workspace_id        = azurerm_log_analytics_workspace.this[each.key].id
+  workspace_id        = module.loganalytics.law_workspace_id.0
   application_type    = "web"
 }
